ec0a929d61647223d70f7ee02ab2b5491d81c948
terry <terry@gmail.com.tw>
2020-08-18 17:29:05 +0800

    my first cat_05:28_8/18

---
+++ catpre/src/components/testhello.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class = "show">
+        {{message}}
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            message: "hellooooooooooo!"
+        }
+    }
+}
+</script>
+
+<style>
+    .show{
+        background: #ff0000;
+        font-size: 20px;
+        text-align: center;
+        color:#fff;
+    }
+</style>
