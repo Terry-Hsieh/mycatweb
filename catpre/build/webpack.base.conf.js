36f89b431882c3723e9edd08c2affe40e4d06565
terry_laptop <a7239703@gmail.com>
2020-08-18 21:09:39 +0800

    fix eslint

--- catpre/build/webpack.base.conf.js
+++ catpre/build/webpack.base.conf.js
@@ -40,7 +40,7 @@ module.exports = {
   },
   module: {
     rules: [
-
+   //   ...(config.dev.useEslint ? [createLintingRule()] : []), fix that=_=
       {
         test: /\.vue$/,
         loader: 'vue-loader',
